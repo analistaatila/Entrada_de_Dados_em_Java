45b4b46ccc60666be39b42d8c8b10b139b7f460a
Atila Batista <atilashow_5@hotmail.com>
2021-09-30 22:41:24 -0300

    Primeiro Commit

---
+++ main.java
@@ -0,0 +1,20 @@
+package main;
+import java.util.Scanner;
+
+public class main {
+	
+		public static void main(String[] args)
+		{
+			Scanner sc = new Scanner(System.in);
+			
+			String x;
+			x = sc.next();
+			System.out.println("Voce digitou: " + x);
+			
+	
+	
+	
+			sc.close();
+		}
+
+}
